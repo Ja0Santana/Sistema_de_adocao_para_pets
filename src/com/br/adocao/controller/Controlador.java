fc6e6387e36ad07b0ca3c45f3c517495129109f2
João Paulo Santana <115142457+Ja0Santana@users.noreply.github.com>
2026-01-07 18:16:37 -0300

    Melhorar a conversão em Pets

--- src/com/br/adocao/controller/Controlador.java
+++ src/com/br/adocao/controller/Controlador.java
@@ -611,7 +611,9 @@ public class Controlador {
     private Pet converterEmPet(Path path) {
         List<String> pet;
         try {
-            pet = Files.readAllLines(path);
+            pet = Files.readAllLines(path).stream()
+                    .filter(linha -> !linha.trim().isEmpty())
+                    .toList();
         }catch (IOException e) {
             throw new RuntimeException("Erro ao ler o arquivo do Pet: " + path, e);
         }
@@ -627,9 +629,9 @@ public class Controlador {
         String linhaEndereco = extrairValor(pet.get(3));
         String[] endPartes = linhaEndereco.split(",");
         String valorIdade = extrairValor(pet.get(4));
-        String idade = valorIdade.equals(".") ? "." : valorIdade.replaceAll("[^0-9]", "");
+        String idade = (valorIdade.equals(".") || valorIdade.isEmpty()) ? "." : valorIdade.replaceAll("[^0-9]", "");
         String valorPeso = extrairValor(pet.get(5));
-        String peso = valorPeso.equals(".") ? "." : valorPeso.replaceAll("[^0-9.]", "");
+        String peso = (valorPeso.equals(".") || valorPeso.isEmpty()) ? "." : valorPeso.replaceAll("[^0-9.]", "");
         String raca = extrairValor(pet.get(6));
         Endereco endereco = new Endereco(deixarSeguro(endPartes, 0), deixarSeguro(endPartes, 1), deixarSeguro(endPartes, 2));
         Pet animal = criarPets(nome, sobrenome, tipo, sexo, idade, peso, raca, endereco);
@@ -652,7 +654,7 @@ public class Controlador {
         if (valor.endsWith(",")) {
             valor = valor.substring(0, valor.length() - 1);
         }
-        return valor.equals("NÃO INFORMADO") ? "." : valor;
+        return (valor.equals("NÃO INFORMADO") || valor.isEmpty()) ? "." : valor;
     }
 
     private String deixarSeguro(String[] partes, int posicao) {
